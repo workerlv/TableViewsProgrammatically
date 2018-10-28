e36b3e7e1517b92332b07ade76a9dac388f959e2
Workerlv <arturs280@inbox.lv>
2018-10-28 12:07:53 +0200

    Initial Commit

---
+++ TableViews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Arturs Vitins on 28/10/2018.
+//  Copyright © 2018 Arturs Vitins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
